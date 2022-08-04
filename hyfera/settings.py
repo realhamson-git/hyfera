7c0cac5a2d4bc772dd67c7c4f56ba5ccabaf35c3
abdul bari <realhamson@gmail.com>
2022-08-04 12:08:01 +0530

    ''

--- hyfera/settings.py
+++ hyfera/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#ft@$his(*yue@ufq4vsya-cju--nhsiigbzjef#9ldg!%38$'
-SHRINK_API = '49dddac5d5687147e961d49342ae8f9cbe45a5d1'
+SECRET_KEY = os.environ['SECRET_KEY']
+SHRINK_API = os.environ['SHRINK_API']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -79,10 +81,11 @@ WSGI_APPLICATION = 'hyfera.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
+DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
 
 
 # Password validation
